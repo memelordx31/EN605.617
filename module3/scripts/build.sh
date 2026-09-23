c9ace0a97f0474595604a0a7c33841ac8b4a396e
memelordx31 <60306306+memelordx31@users.noreply.github.com>
2026-09-22 22:59:23 -0400

    Update build.sh

--- module3/scripts/build.sh
+++ module3/scripts/build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
-CONFIG_FILE="${1:-$ROOT_DIR/config.yaml}"
+CONFIG_FILE="${1:-$ROOT_DIR/assignment_config.yaml}"
 GET="python3 $ROOT_DIR/get_config.py $CONFIG_FILE"
 
 SRC_DIR=$($GET project.src_dir)
